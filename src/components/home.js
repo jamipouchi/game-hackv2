0d7fe7d0e1f395b03d6a2b6ab506e4b9325e9712
jamipouchi <miquelpuigturon@gmail.com>
2022-04-30 10:41:57 +0200

    Home en proces

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import './home.css'
 
-export default function Home() {
-    return(
-        <div>
-            <h1>HOME</h1>
+import cup from '../assets/cup.png'
+import snake from '../assets/427533.png'
+import killer from '../assets/7-512.webp'
+
+
+export default function Home(props) {
+    return (
+        <div className='container'>
+            <h4 className='title'>Available games</h4>
+            <Carousel variant='dark'>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100"
+                        src={cup}
+                        alt="Image One"
+                    />
+                    <Carousel.Caption>
+                        <h3><a className="nav-link" href="ranking">Play!</a></h3>
+                        <p>Sample Text for Image One</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100"
+                        src={snake}
+                        alt="Image Two"
+                    />
+                    <Carousel.Caption>
+                        <h3>Label for second slide</h3>
+                        <p>Sample Text for Image Two</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100"
+                        src={killer}
+                        alt="Image Three"
+                    />
+                    <Carousel.Caption>
+                        <h3>Label for third slide</h3>
+                        <p>Sample Text for Image Three</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
-    )
+    );
 }
